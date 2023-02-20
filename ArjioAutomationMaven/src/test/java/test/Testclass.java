883e2e46449b7f2d77df11ffdc943922a554a1b5
Kunal2000T <125537702+Kunal2000T@users.noreply.github.com>
2023-02-20 22:19:15 +0530

    Update Testclass.java

--- ArjioAutomationMaven/src/test/java/test/Testclass.java
+++ ArjioAutomationMaven/src/test/java/test/Testclass.java
@@ -2,8 +2,9 @@ package test;
 
 public class Testclass {
 
-	public static void main(String[] args)  {
+	//public static void main(String[] args)  {
 		
+	public static void main(String[] args)  {
 	
 		
 		
