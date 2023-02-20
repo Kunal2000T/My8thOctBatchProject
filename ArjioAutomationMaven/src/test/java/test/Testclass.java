9abca2ea2b262ae6d72ed3bbd2455dbf92315250
Kunal2000T <125537702+Kunal2000T@users.noreply.github.com>
2023-02-20 22:24:07 +0530

    Update Testclass.java

--- ArjioAutomationMaven/src/test/java/test/Testclass.java
+++ ArjioAutomationMaven/src/test/java/test/Testclass.java
@@ -1,10 +1,9 @@
 package test;
 
 public class Testclass {
-
-	//public static void main(String[] args)  {
 		
-	public static void main(String[] args)  {
+	public static void main(String[] args)  { 
+		driver.get();
 	
 		
 		
